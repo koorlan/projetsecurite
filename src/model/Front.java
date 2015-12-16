825985d7d02b623edb0f1048e00d28dab095fedb
Korlan Colas <korlan.colas@insa-cvl.fr>
2015-12-16 12:48:39 +0100

    Just for test class. change this in afternoon

---
+++ src/model/Front.java
@@ -0,0 +1,32 @@
+package model;
+public class Front{
+	private String name;
+	private String ip;
+	private int port;
+	
+public Front(String name, String ip, int port) {
+		super();
+		this.name = name;
+		this.ip = ip;
+		this.port = port;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	public int getPort() {
+		return port;
+	}
+	public void setPort(int port) {
+		this.port = port;
+	}
+	
+}
