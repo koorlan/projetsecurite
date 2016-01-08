7fd768eb2378cc1f68c00c857b814fd5d559ab33
TainaBouganim <bouganim.taina@gmail.com>
2016-01-08 18:29:53 +0100

    suppression des noeuds tor dans DatabaseMain

--- src/initialisation_BD/DatabaseMain.java
+++ src/initialisation_BD/DatabaseMain.java
@@ -41,7 +41,7 @@ public class DatabaseMain {
 
 			//Rajout des colonnes (en rouge dans le PPT)
 			RunQuery(stmt1, QUERIES[6]); //Rajoute le champ Ksec à la table Types_Utili
-			RunQuery(stmt1, QUERIES[67]); //On cree la colonne Liste_Groupe dans la table Utilisateurs
+			RunQuery(stmt1, QUERIES[7]); //On cree la colonne Liste_Groupe dans la table Utilisateurs
 			RunQuery(stmt1, QUERIES[8]); //On cree la colonne Metadonnees dans la table Utilisateurs
 			RunQuery(stmt1, QUERIES[9]); //On cree la colonne Meta_Chiffrees dans la table Types_Utili
 			RunQuery(stmt1, QUERIES[10]); //On cree la colonne Valeur_Chiffree dans la table Donnees
@@ -126,7 +126,7 @@ public class DatabaseMain {
 				Meta=login2 + "," + Liste_Groupe + statut + "," + affectation;
 				rss1.close();
 				RunQuery(stmt1, QUERIES[19] + Meta + QUERIES[20] + login2 + "'"); 
-				RunQuery(stmt1, QUERIES[68] + Liste_Groupe + QUERIES[20] + login2 + "'");
+				RunQuery(stmt1, QUERIES[35] + Liste_Groupe + QUERIES[20] + login2 + "'");
 			}
 			rss.close();
 
@@ -153,7 +153,7 @@ public class DatabaseMain {
 			// 8 - CALCUL DE E_Cred_KSec: CHIFFREMENT DE KSec avec les Kpub (CREDENTIALS) DANS LA TABLE Types_Utili
 			//-------------------------------------------------------------------------------------------------------
 
-			RunQuery(stmt1, QUERIES[7]);// Cree une version de Cles_Types avec KSec
+			RunQuery(stmt1, QUERIES[29]);// Cree une version de Cles_Types avec KSec
 			rss = stmt1.executeQuery(QUERIES[23]);// recupere KSec et Cred_Auto_Ref
 			while (rss.next()==true){
 				String KsecString= rss.getString("Ksec");
@@ -184,7 +184,7 @@ public class DatabaseMain {
 
 			// 9 - CREATION D'UNE BD TEMPORAIRE
 			//-------------------------------------------------------------------------------------------------------
-			// On copie les tables contenant les noeuds, users et frontales dans une BD TEMP
+			// On copie les tables contenant les users et les frontales dans une BD TEMP
 			// Pour pouvoir la parcourir (et en même temps attacher la BD originale) car sinon
 			// on a des problemes de verrouillage de la BD pendant le parcours
 			Connection con4=DBManager.OpenCreateDB(PREFIX + "/TEMP.sqlite");
@@ -192,7 +192,6 @@ public class DatabaseMain {
 			RunQuery(stmt4, QUERIES[26]); //attache initDB
 			RunQuery(stmt4, QUERIES[27]); //Copie de la table utilisateurs
 			RunQuery(stmt4, QUERIES[28]); //Copie de la table frontales
-			RunQuery(stmt4, QUERIES[29]); //Copie de la table des NoeudsTOR
 			RunQuery(stmt4, QUERIES[30]); //Copie de la table des Types_Utilisateurs
 			RunQuery(stmt4, QUERIES[31]); //Copie de la table des cles
 			RunQuery(stmt4, QUERIES[32]); //Detache initDB
@@ -221,7 +220,6 @@ public class DatabaseMain {
 				RunQuery(stmt2, QUERIES[46] + login +"'"); //Copie des credentials autorises de l'utilisateur
 				RunQuery(stmt2, QUERIES[47] + login +"'"); //Copie du login/passwd/ip/port de l'utilisateur
 				RunQuery(stmt2, QUERIES[48] + login +"'"); //Copie de la frontale de l'utilisateur
-				RunQuery(stmt2, QUERIES[29]); //Copie de la tables des NoeudsTOR
 				RunQuery(stmt2, QUERIES[32]); //Detache initDB
 
 				// Creation de la BD non securisee pour l'utilisateur
@@ -231,15 +229,14 @@ public class DatabaseMain {
 				RunQuery(stmt3, QUERIES[49] + login +"'"); // Types
 				RunQuery(stmt3, QUERIES[50] + login +"'"); //Donnees_Chiffree
 				RunQuery(stmt3, QUERIES[51] + login +"'"); //Cles_Types
-				RunQuery(stmt3, QUERIES[69] + login +"'"); //Utilisateurs
-				
+				RunQuery(stmt3, QUERIES[37] + login +"'"); //Utilisateurs
 				RunQuery(stmt3, QUERIES[32]); //Detache initDB
 
 			}
 
 			// 11 - CREATION DES FRONTALES
 			//-------------------------------------------------------------------------------------------------------
-			ResultSet rs2 = stmt4.executeQuery(QUERIES[63]); // Recupere les Frontales
+			ResultSet rs2 = stmt4.executeQuery(QUERIES[39]); // Recupere les Frontales
 			String frontale =null;
 			while (rs2.next()==true){
 				frontale = rs2.getString("Frontale");
@@ -268,33 +265,17 @@ public class DatabaseMain {
 			RunQuery(stmt6, QUERIES[61]);  //Table Server
 			RunQuery(stmt6, QUERIES[32]); //Detache initDB
 
-	
-			// 13 - CREATION DES NOEUDS TOR
-			//-------------------------------------------------------------------------------------------------------
-			ResultSet rs4 = stmt4.executeQuery(QUERIES[62]); // Recupere les noeuds TOR
-			String NoeudTOR =null;
-			while (rs4.next()==true){
-				NoeudTOR = rs4.getString("NoeudTOR");
-				Connection con7=DBManager.OpenCreateDB(PREFIX + "/" + NoeudTOR + ".sqlite");
-				Statement stmt7=DBManager.CreateStatement(con7);
-				RunQuery(stmt7, QUERIES[26]); //attache initDB
-				RunQuery(stmt7, QUERIES[59]); //Table Routage
-				RunQuery(stmt7, QUERIES[29]); //Table NoeudsTOR
-				RunQuery(stmt7, QUERIES[32]); //Detache initDB
-			}
-			rs4.close();
 
-			// 14 - CREATION DE LA BASE POUR LE LANCEMENT
+			// 13 - CREATION DE LA BASE POUR LE LANCEMENT
 			//-------------------------------------------------------------------------------------------------------
-			Connection con8=DBManager.OpenCreateDB(PREFIX + "/Lancement.sqlite");
-			Statement stmt8=DBManager.CreateStatement(con8);
-			RunQuery(stmt8, QUERIES[26]); //attache initDB
-			RunQuery(stmt8, QUERIES[64]); //Copie de la liste des utilisateurs
-			RunQuery(stmt8, QUERIES[65]); //Copie de la liste des frontales
-			RunQuery(stmt8, QUERIES[66]); //Copie de la liste des Noeuds TOR
-			RunQuery(stmt8, QUERIES[32]); //Detache initDB
+			Connection con7=DBManager.OpenCreateDB(PREFIX + "/Lancement.sqlite");
+			Statement stmt7=DBManager.CreateStatement(con7);
+			RunQuery(stmt7, QUERIES[26]); //attache initDB
+			RunQuery(stmt7, QUERIES[62]); //Copie de la liste des utilisateurs
+			RunQuery(stmt7, QUERIES[63]); //Copie de la liste des frontales
+			RunQuery(stmt7, QUERIES[32]); //Detache initDB
 
-			// 15 - SUPPRESSION DES BASES INIT_DB COMPLETE ET DE LA BASE TEMP
+			// 14 - SUPPRESSION DES BASES INIT_DB COMPLETE ET DE LA BASE TEMP
 			//-------------------------------------------------------------------------------------------------------
 			Runtime.getRuntime().exec("rm " + INIT_FILE); 
 			Runtime.getRuntime().exec("rm " + PREFIX + "/TEMP.sqlite");
@@ -337,7 +318,7 @@ public class DatabaseMain {
 			"ALTER TABLE Cred_Autorise ADD COLUMN ID_Cred INTEGER",//4
 			"UPDATE Cred_Autorise set ID_Cred = random()",//5
 			"ALTER TABLE Types_Utili ADD COLUMN Ksec TEXT",//6
-			"CREATE TABLE Cles_Types AS select Login, Type, Ksec, Cred_Auto_Ref, '' as E_Cred_Ksec FROM Types_Utili T, Cred_Autorise C where T.politique=C.politique",//7
+			"ALTER TABLE Utilisateurs ADD COLUMN Liste_Groupe TEXT",//7
 			"ALTER TABLE Utilisateurs ADD COLUMN Metadonnees TEXT",//8
 			"ALTER TABLE Types_Utili ADD COLUMN Meta_Chiffrees TEXT",//9
 			"ALTER TABLE Donnees ADD COLUMN Valeur_Chiffree TEXT",//10
@@ -361,18 +342,19 @@ public class DatabaseMain {
 			"ATTACH '"+ INIT_FILE + "' as 'BASE'",//26
 			"create table Utilisateurs as select * from BASE.Utilisateurs",//27
 			"create table Frontales as select * from BASE.Frontales",//28
-			"create table NoeudsTOR as select * from BASE.NoeudsTOR",//29
+			"CREATE TABLE Cles_Types AS select Login, Type, Ksec, Cred_Auto_Ref, '' as E_Cred_Ksec FROM Types_Utili T, Cred_Autorise C where T.politique=C.politique",//29
 			"create table Types_Utili as select * from BASE.Types_Utili",//30
 			
 			"create table Cles as select * from BASE.cles",//31
 			"DETACH 'BASE';",//32
 			"SELECT Login, Icone FROM Utilisateurs",//33
 			"Update Cles SET Kpriv = NULL WHERE ID_Cle not in (SELECT ID_Cle from BASE.Statuts S, BASE.Utilisateurs U where U.ID_Statut = S.ID_Statut and Login = '",//34
-			"",
+			"UPDATE Utilisateurs SET Liste_Groupe = '",//35
 			"UNION SELECT ID_Cle from BASE.Affectations A, BASE.Utilisateurs U where U.ID_Affectation = A.ID_Affectation and Login = '",//36
-			"",
+			"CREATE TABLE Utilisateurs AS SELECT U.Ip, U.Port, S.Statut_Gen, A.Affect_Gen FROM BASE.Utilisateurs U, BASE.Affectations A, BASE.Statuts S "
+			+ "WHERE U.ID_Statut = S.ID_Statut AND U.ID_Affectation = A.ID_Affectation AND Login = '",//37
 			"UNION SELECT ID_Cle from BASE.Groupes G, BASE.Inscription I where I.ID_Groupe = G.ID_Groupe and Login = '",//38
-			"",
+			"SELECT Frontale, Icone from Frontales",//39
 			"create table Groupes as select * from BASE.Groupes",//40
 			
 			"create table Statuts as select * from BASE.Statuts",//41
@@ -405,14 +387,13 @@ public class DatabaseMain {
 			
 			
 			"CREATE TABLE Server as SELECT * from BASE.Server ",//61
-			"SELECT NoeudTOR, Icone from NoeudsTOR",//62
-			"SELECT Frontale, Icone from Frontales", //63
-			"CREATE Table Utilisateurs as SELECT Login, Icone from BASE.Utilisateurs",//64
-			"CREATE Table Frontales as SELECT Frontale, Icone from BASE.Frontales",//65
-			"CREATE Table NoeudsTOR as SELECT NoeudTOR, Icone from BASE.NoeudsTOR",//66
-			"ALTER TABLE Utilisateurs ADD COLUMN Liste_Groupe TEXT",//67
-			"UPDATE Utilisateurs SET Liste_Groupe = '",//68
-			"CREATE TABLE Utilisateurs AS SELECT U.Ip, U.Port, S.Statut_Gen, A.Affect_Gen FROM BASE.Utilisateurs U, BASE.Affectations A, BASE.Statuts S "
-			+ "WHERE U.ID_Statut = S.ID_Statut AND U.ID_Affectation = A.ID_Affectation AND Login = '",//69
+			"CREATE Table Utilisateurs as SELECT Login, Icone from BASE.Utilisateurs",//62
+			"CREATE Table Frontales as SELECT Frontale, Icone from BASE.Frontales",//63
+			"",//64
+			"",//65
+			"",//66
+			"",//67
+			"",//68
+			"",//69
 			""};
 }
